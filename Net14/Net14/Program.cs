17bfb07696ed0397666a5b0ae52a719448b6c3f7
JinOptimist <88jin@mail.ru>
2022-01-25 19:55:29 +0300

    Init

---
+++ Net14/Net14/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Net14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hi {name} nice to see you.");
+
+            int number = EnterNumber("First player enter a number");
+
+            Console.Clear();
+
+            Console.WriteLine("Good day second player");
+
+            int secondNumber = EnterNumber("Second player enter...");
+
+            while (secondNumber != number)
+            {
+                if (number > secondNumber)
+                {
+                    // true
+                    Console.WriteLine(">");
+                }
+                else
+                {
+                    // false
+                    Console.WriteLine("<");
+                }
+
+                secondNumber = EnterNumber("Second player enter...");
+            }
+
+            Console.WriteLine("Win");
+
+
+        }
+
+        private static int EnterNumber(string message)
+        {
+            Console.WriteLine(message);
+            var numberString = Console.ReadLine();
+            int number = Int32.Parse(numberString);
+
+            return number;
+        }
+    }
+}
