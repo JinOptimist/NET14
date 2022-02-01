5a037f51a94c6ecaaab3b7195b080d23395fc5cd
dimamay3r@gmail.com <dimamay3r@gmail.com>
2022-02-01 20:26:48 +0300

    Test

---
+++ Net14/Net14/Apple.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net14
+{
+    internal class Apple
+    {
+        private  int Long { get; set; }
+        private  string Name { get; set; }
+        private Apple(int Long, string Name)
+        {
+
+        }
+    }
+}
