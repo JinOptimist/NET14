7fb6ccc6fb378d899961b06337768cbdd842f5e0
RusikKuznec <ruslan00980@mail.com>
2022-02-01 20:36:59 +0300

    Add class Pentagramma

---
+++ Net14/Net14/Pentagramma.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net14
+{
+    class Pentagramma
+    {
+        public int Men { get; set; }
+        public int Cost { get; set; }
+        public int Grand { get; set; }
+    }
+}
