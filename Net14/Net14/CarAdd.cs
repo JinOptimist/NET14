5f6a8f91b6c9441e9e2caae119a12c31dcdfefec
andrejdesukevic@gmail.com <andrejdesukevic@gmail.com>
2022-02-01 20:17:59 +0300

    Добавил машину №4

---
+++ Net14/Net14/CarAdd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net14
+{
+    class CarAdd
+    {
+        private string Name { get; set; }
+        private int Power { get; set; }
+        private int MaxSpeed { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine(Name, Power, MaxSpeed);
+        }
+    }
+}
