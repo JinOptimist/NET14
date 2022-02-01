562a43b648e8ae6a66f8942dc195b18692102cb9
JinOptimist <88jin@mail.ru>
2022-02-01 20:01:18 +0300

    Add rocket

---
+++ Net14/Net14/TestClass/Rocket.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net14.TestClass
+{
+    class Rocket
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+    }
+}
