8f193b4ea29a96b7baace083eddcd707c1f6009a
Журавлев Алексей <alehandro.200351@gmail.com>
2022-02-11 20:14:10 +0300

    add ICharacter

--- Net14/Net14/Maze/Cells/ChestOfLuck.cs
+++ Net14/Net14/Maze/Cells/ChestOfLuck.cs
@@ -9,7 +9,7 @@ namespace Net14.Maze.Cells
         public override char Symbol => '■';
         public override ConsoleColor Color => ConsoleColor.DarkYellow;
 
-        public override bool TryToStep(Сharacter hero)
+        public override bool TryToStep(IСharacter hero)
         {
             Random rand = new Random();
             int num = rand.Next(0, 41);
