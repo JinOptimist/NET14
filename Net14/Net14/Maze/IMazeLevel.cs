ea0740047021df63ea02443bc7a3a5e0837432a3
JinOptimist <88jin@mail.ru>
2022-02-11 20:58:25 +0300

    Add IMazLevel

---
+++ Net14/Net14/Maze/IMazeLevel.cs
@@ -0,0 +1,16 @@
+﻿using Net14.Maze.Cells;
+using System.Collections.Generic;
+
+namespace Net14.Maze
+{
+    public interface IMazeLevel
+    {
+        List<BaseCell> Cells { get; set; }
+        int Height { get; set; }
+        Сharacter Hero { get; set; }
+        int Width { get; set; }
+
+        void Move(Direction left);
+        void ReplaceCell(BaseCell cell);
+    }
+}
