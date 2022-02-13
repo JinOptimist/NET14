3fa2cd5e9577cfd0141619cf85c8cb6755395d02
kirill22012002 <perepechkin_2002@mail.ru>
2022-02-13 21:04:47 +0300

    Add GroundTest

---
+++ Net14/Net14.Tests/Maze/Cells/GroundTest.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using Net14.Maze;
+using Net14.Maze.Cells;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net14.Tests.Maze.Cells
+{
+    public class GroundTest
+    {
+        [Test]
+        public void TryToStep_CanStep()
+        {
+            // Подготовка
+            var mazeMock = new Mock<IMazeLevel>();
+            var ground = new Ground(mazeMock.Object);
+            var heroMock = new Mock<IСharacter>();
+
+            //Действие
+            var answer = ground.TryToStep(heroMock.Object);
+
+            //Проверка
+            Assert.AreEqual(true, answer);
+        }
+
+        [Test]
+        public void Symbol()
+        {
+            // Подготовка
+            var mazeMock = new Mock<IMazeLevel>();
+            var ground = new Ground(mazeMock.Object);
+
+            //Проверка
+            Assert.AreEqual('.', ground.Symbol);
+        }
+
+    }
+}
