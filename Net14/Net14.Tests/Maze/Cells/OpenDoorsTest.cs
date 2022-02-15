1078a99de68e2227cd7f0caa6a9b147610245b6a
kirill22012002 <perepechkin_2002@mail.ru>
2022-02-15 14:51:03 +0300

    Add Open Doors Test

---
+++ Net14/Net14.Tests/Maze/Cells/OpenDoorsTest.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using Net14.Maze;
+using Net14.Maze.Cells;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net14.Tests.Maze.Cells
+{
+    public class OpenDoorsTest
+    {
+        [Test]
+        public void TryToStep_CanStep()
+        {
+            // Подготовка
+            var mazeMock = new Mock<IMazeLevel>();
+            var openDoors = new OpenDoors(mazeMock.Object);
+            var heroMock = new Mock<IСharacter>();
+
+            //Действие
+            var answer = openDoors.TryToStep(heroMock.Object);
+
+            //Проверка
+            Assert.AreEqual(true, answer);
+        }
+        [Test]
+        public void Symbol()
+        {
+            // Подготовка
+            var mazeMock = new Mock<IMazeLevel>();
+            var openDoors = new OpenDoors(mazeMock.Object);
+
+
+            //Проверка
+            Assert.AreEqual('/', openDoors.Symbol);
+        }
+    }
+}
