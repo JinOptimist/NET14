8f94a718bac83f5e934a3c33ce9a90e6f3a7261d
kirill22012002 <perepechkin_2002@mail.ru>
2022-02-15 21:30:09 +0300

    AddRegistration

--- Net14/TeamSocial/Social.cs
+++ Net14/TeamSocial/Social.cs
@@ -7,16 +7,42 @@ namespace TeamSocial
 {
     public class Social
     {
-        new public List<User> users { get; set;}
+        new public List<User> users { get; set; }
         public void Autorization()
         {
 
         }
 
-        public void Registration()
+        public void Registration(string firstName, string lastName, string email, int age, string password)
         {
+            var FirstName = firstName;
+            var LastName = lastName;
+            var Email = email;
+            var Age = age;
+            var Password = password;
+
+            var user = new User()
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Email = Email,
+                Age = Age,
+                Password = Password
+
+            };
+        }
+
+        public void DataOutput()
+        {
+            var FirstName = Console.ReadLine();
+            var LastName = Console.ReadLine();
+            var Email = Console.ReadLine();
+            var Age = Int32.Parse(Console.ReadLine());
+            var Password = Console.ReadLine();
+            Registration(FirstName, LastName, Email, Age, Password);
 
         }
+
 
 
     }
