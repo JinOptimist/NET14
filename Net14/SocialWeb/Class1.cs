11dcd7502afcae38b5fa31ffa5209e0c94c4a903
Журавлев Алексей <alehandro.200351@gmail.com>
2022-02-15 20:21:23 +0300

    TeamSocial AddUserClass

--- Net14/SocialWeb/Class1.cs
+++ Net14/SocialWeb/Class1.cs
@@ -2,7 +2,22 @@
 
 namespace SocialWeb
 {
-    public class Class1
+    public class User
     {
+        public User(string firstName, string lastName, string email, int age, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Age = age;
+            Password = password;
+
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Password { get; set; }
+
     }
 }
