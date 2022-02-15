4579eab2950b347e70c69d9163e4118de410b284
ANTON <tse.toneee@gmail.com>
2022-02-15 19:43:04 +0300

    Add project

---
+++ Net14/Team_Store/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Team_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
