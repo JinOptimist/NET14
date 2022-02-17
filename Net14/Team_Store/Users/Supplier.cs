0d20fe47d6dd99ed3596edd02badcf6f87c9cb8e
andrejdesukevic@gmail.com <andrejdesukevic@gmail.com>
2022-02-18 01:19:43 +0300

    Добавил Пользователей

---
+++ Net14/Team_Store/Users/Supplier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Team_Store.Users
+{
+    public class Supplier:IBaseUser
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+        public string Password { get; set; }
+        public string IDUser { get; set; }
+       
+    }
+}
