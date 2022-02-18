39f1028e451f877f89875a0ff0ee7f5631a6cde7
Angelegna <angelv87@tut.by>
2022-02-18 21:23:09 +0300

    Create_list_Post

--- Net14/TeamSocial/PostWall.cs
+++ Net14/TeamSocial/PostWall.cs
@@ -8,10 +8,14 @@ namespace TeamSocial
     {
         public void CreatePostWall()
         {
-            List<Post> posts = new List<Post>();
-            posts.
-        }
+            List<Post> wall = new List<Post>();
+            Post post = new Post();
 
-
-    }
+            for (int i = 0; i < wall.Count; i++)
+            {
+              public string post.
+            }
+        }    
+    }   
+  
 }
