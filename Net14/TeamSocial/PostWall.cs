164e58e4a5bf6446b7f8f0ba76dec3cd5cca62f2
Angelegna <angelv87@tut.by>
2022-02-18 21:03:47 +0300

    Task_183_Create_Class

---
+++ Net14/TeamSocial/PostWall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamSocial
+{
+    public class PostWall
+    {
+        public void CreatePostWall()
+        {
+            List<Post> posts = new List<Post>();
+            posts.
+        }
+
+
+    }
+}
