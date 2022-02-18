6c691d69e5c059422c82a02e10c947313c2b8c72
ANTON <tse.toneee@gmail.com>
2022-02-18 21:04:59 +0300

    Add stock and goods

--- Net14/Team_Store/Stock.cs
+++ Net14/Team_Store/Stock.cs
@@ -6,5 +6,29 @@ namespace Team_Store
 {
     class Stock
     {
+
+        public List<Goods> goods { get; set; } = new List<Goods>();
+
+        public void Add_Good()
+        {
+            var thing_to_stock = new Goods();
+            thing_to_stock.product_description();
+            goods.Add(thing_to_stock);
+
+        }
+
+
+
+        public void Show_Stock()
+        {
+            foreach (var item in goods)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Price);
+                Console.WriteLine(item.Amount);
+                Console.WriteLine(item.Category);
+            }
+        }
     }
 }
+
