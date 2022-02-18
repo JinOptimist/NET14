515808b9d11371867aadd8436eadec19e3cafec7
theGrubby <mark.zhdanovich@mail.ru>
2022-02-18 21:46:51 +0300

    Add methods without realisation

--- Net14/Team_Store/Basket.cs
+++ Net14/Team_Store/Basket.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Team_Store
 {
     public class Basket
     {
-        private List<Goods> Goods_in_Basket = new List<Goods>();
+        public List<Goods> Goods_in_Basket { get; set; }
         public void Add(Goods good)
         {
-            if (Goods_in_Basket.Contains(good) == false)
-            {
-                Goods_in_Basket.Add(good);
-            }
+            
+        }
             good.Amount_in_Basket++;
         }
         public void Remove(Goods good)
@@ -21,7 +20,7 @@ namespace Team_Store
             
         }
         public void Show()
-        {            
+        {
             if (Goods_in_Basket.Count == 0)
             {
                 Console.WriteLine("Your basket is empty.");
@@ -36,25 +35,21 @@ namespace Team_Store
                     var goodsList =
                         $"{good.Amount_in_Basket}x {good.Name}\t Price: {good.Price}\t Cumulative price: {good.Price * good.Amount_in_Basket}";
                     Console.WriteLine(goodsList);
-
+            
                     cumulativePrice += good.Price * good.Amount_in_Basket;
                 }
                 var message =
                     $"\nCumulative price: {cumulativePrice}";
                 Console.WriteLine(message);
-            }
+        }
         }
         public void Clear()
         {
-            for (int i = Goods_in_Basket.Count - 1; i >= 0; i--)
-            {
-                Goods_in_Basket[i].Amount_in_Basket = 0;
-                Goods_in_Basket.Remove(Goods_in_Basket[i]);
-            }
+            
         }
         public void Buy()
         {
-            //Show();
+            
         }
     }
 }
