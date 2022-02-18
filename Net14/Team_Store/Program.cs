387ca26bb5a799a4bdbd23388cc16331548b1db0
ANTON <tse.toneee@gmail.com>
2022-02-18 21:04:59 +0300

    Add stock and goods

--- Net14/Team_Store/Program.cs
+++ Net14/Team_Store/Program.cs
@@ -6,26 +6,9 @@ namespace Team_Store
     {
         static void Main(string[] args)
         {
-            //var basket = new Basket();
-            //Goods good1 = new Goods("Audi", 10, "Cars", 1000);
-            //Goods good2 = new Goods("Opel", 5, "Cars", 3000);
-            //Goods good3 = new Goods("BMW", 2, "Cars", 5000);
-            //Goods good4 = new Goods("iPhone", 10, "Phones", 1000);
-            //Goods good5 = new Goods("Samsung", 10, "Phones", 1000);
-            //Goods good6 = new Goods("Xiaomi", 100, "Phones", 500);
-            //Goods good7 = new Goods("Cake", 2, "Products", 10);
-            //Goods good8 = new Goods("Sausage", 10, "Products", 5);
-            //Goods good9 = new Goods("Cucumber", 23, "Products", 3);
-                               
-            //basket.Add(good8);
-            //basket.Add(good8);
-            //basket.Add(good3);
-            //basket.Clear();
-            //basket.Show();
-            //Console.ReadKey();
-              
-            //var menu = new Menu();
-            //menu.Start();           
+            var menu = new Menu();
+            menu.Start();
+           
         }
     }
 }
