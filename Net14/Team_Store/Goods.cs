e962da2984afb85e0c418a0aa9d1ccf6a06a94c6
ANTON <tse.toneee@gmail.com>
2022-02-18 20:38:47 +0300

    Combine

--- Net14/Team_Store/Goods.cs
+++ Net14/Team_Store/Goods.cs
@@ -4,31 +4,8 @@ using System.Text;
 
 namespace Team_Store
 {
-    public class Goods
+    class Goods
     {
-        public string Name { get; set; }
-        public int Amount { get; set; }
-        public string Category { get; set; }
-        public int Price { get; set; }
-        public int Amount_in_Basket { get; set; }
-
-
-        public Goods(string name, int amount, string category, int price)
-        {
-            Name = name;
-            Amount = amount;
-            Category = category;
-            Price = price;
-            Amount_in_Basket = 0;
-        }
-
-        public Goods()
-        {
-
-        }
-
-        public void product_description()
-        {
             Console.WriteLine("Enter Goods:\n\t 1) Name\n\t 2) Amount\n\t 3) Category\n\t 4) Price ");
 
 
