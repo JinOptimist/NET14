e979a2ea0f844ed1bf87e389a783be69874a953b
theGrubby <mark.zhdanovich@mail.ru>
2022-02-19 02:24:16 +0300

    Added basket Add/Remove/Show - working Clear - not working Slighly changes in Goods and Main

--- Net14/Team_Store/Basket.cs
+++ Net14/Team_Store/Basket.cs
@@ -1,37 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Team_Store
 {
     public class Basket
     {
-        public List<Goods> Goods_in_Basket { get; set; }
         private List<Goods> Goods_in_Basket = new List<Goods>();
         public void Add(Goods good)
         {
-            
             if (Goods_in_Basket.Contains(good) == false)
             {
                 Goods_in_Basket.Add(good);
-        }
-            good.Amount_in_Basket++;
+            }
+                good.Amount_in_Basket++;
         }
         public void Remove(Goods good)
         {
-            good.Amount_in_Basket = 0;
             Goods_in_Basket.Remove(good);
-            
+            good.Amount_in_Basket = 0;
         }
         public void Show()
         {
+            //Нужно с табуляциями что-то придумать
             if (Goods_in_Basket.Count == 0)
             {
                 Console.WriteLine("Your basket is empty.");
             }
             else
             {
-                //Нужно с табуляциями что-то придумать
                 Console.WriteLine("Your basket contains:");
                 var cumulativePrice = 0;
                 foreach (var good in Goods_in_Basket)
@@ -39,26 +35,24 @@ namespace Team_Store
                     var goodsList =
                         $"{good.Amount_in_Basket}x {good.Name}\t Price: {good.Price}\t Cumulative price: {good.Price * good.Amount_in_Basket}";
                     Console.WriteLine(goodsList);
-            
+
                     cumulativePrice += good.Price * good.Amount_in_Basket;
-        }
+                }
                 var message =
                     $"\nCumulative price: {cumulativePrice}";
                 Console.WriteLine(message);
-        }
+            }
         }
         public void Clear()
         {
-            
-            for (int i = Goods_in_Basket.Count - 1; i >= 0; i--)
+            foreach (var good in Goods_in_Basket)
             {
-                Goods_in_Basket[i].Amount_in_Basket = 0;
-                Goods_in_Basket.Remove(Goods_in_Basket[i]);
+                Goods_in_Basket.Remove(good);
+                good.Amount_in_Basket = 0;
             }
         }
         public void Buy()
         {
-            
             //Show();
         }
     }
