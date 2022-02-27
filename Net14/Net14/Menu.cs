7711b0d719e1c79ffc0332ee1330ec4a967e79a8
rusikkuznec <ruslan00980@mail.ru>
2022-02-27 21:58:43 +0300

    Real add notes

--- Net14/Net14/Menu.cs
+++ Net14/Net14/Menu.cs
@@ -107,15 +107,12 @@ namespace Net14
             specialList.UserDay = new List<SpecialDay>();
             var cheker = new MonthLevel();
             bool stillWatch = true;
+            var noNotes = "No notes for today.";
             while (stillWatch)
             {
                 var creater = createCalendar.Create(monthLevel.DaysInWeek, monthLevel.WeeksInMonth, monthLevel.DayNumber,
                     monthLevel.EmptyDays, monthLevel.MonthNumber, monthLevel.Year);
-                List<SpecialDay> count = specialList.UserDay.FindAll(cell => cell.Year == creater.Year && cell.Month == creater.MonthNumber);
-                if (count.Count != 0)
-                {
-                    creater = CheckCalendar(specialList, creater, count);
-                } 
+                creater = CheckCalendar(specialList, creater, noNotes);
                 calendarDrawer.Draw(creater);
                 var key = Console.ReadKey();
                 switch (key.Key)
@@ -141,6 +138,8 @@ namespace Net14
                         }
                         break;
                     case ConsoleKey.Spacebar:
+                        bool boolForNote1 = true;
+                        bool boolForNote2 = true;
                         var mess = "\nEnter date in format dd.mm.yyyy:\n";
                         var userDate = EnterDate(mess);
                         Console.Clear();
@@ -148,39 +147,48 @@ namespace Net14
                         monthLevel.Year = userDate.Year;
                         var monthLevelForNote = createCalendar.Create(monthLevel.DaysInWeek, monthLevel.WeeksInMonth, monthLevel.DayNumber,
                         monthLevel.EmptyDays, monthLevel.MonthNumber, monthLevel.Year);
-
-                        if (count.Count != 0)
+                        while (boolForNote1)
                         {
-                            monthLevelForNote = CheckCalendar(specialList, creater, count);
-                        }
-
-                        var noteDay = monthLevelForNote.Month.FindAll(x => x.Symbol == userDate.Day.ToString());
-                        Console.Clear();
-                        Console.Write($"Your choosed: {userDate.ToShortDateString()}.  \n{noteDay[0].Note}\n\n");
-                        Console.Write("Add note for this day?  \nPress \"Y\" or \"N\".\n");
-                        var key2 = Console.ReadKey();
-                        switch (key2.Key)
-                        {
-                            case ConsoleKey.Y:
-                                Console.Clear();
-                                Console.Write("Enter your note: \n");
-                                var note = Console.ReadLine();
-                                var datee = new SpecialDay()
+                            monthLevelForNote = CheckCalendar(specialList, monthLevelForNote, noNotes);
+                            var noteDay = monthLevelForNote.Month.Find(x => x.Symbol == userDate.Day.ToString());
+                            Console.Clear();
+                            Console.Write($"Your choosed: {userDate.ToShortDateString()}.  \n{noteDay.Note}\n\n");
+                            Console.Write("Add note for this day?  \nPress \"Y\" .... \"N\"....\"X\".\n");
+                            boolForNote2 = true;
+                            while (boolForNote2)
+                            {
+                                var key2 = Console.ReadKey();
+                                switch (key2.Key)
                                 {
-                                    Day = userDate.Day,
-                                    Month = userDate.Month,
-                                    Year = userDate.Year,
-                                    Note = note
-                                };
-                                specialList.UserDay.Add(datee);
-                                monthLevel.MonthNumber = DateTime.Now.Month;
-                                monthLevel.Year = DateTime.Now.Year;
-                                break;
-                            case ConsoleKey.N:
-                                break;
+                                    case ConsoleKey.Y:
+                                        Console.Clear();
+                                        Console.Write("Enter your note: \n");
+                                        var note = Console.ReadLine();
+                                        specialList = AddNotes(specialList, userDate, note, noNotes);
+                                        monthLevel.MonthNumber = DateTime.Now.Month;
+                                        monthLevel.Year = DateTime.Now.Year;
+                                        boolForNote1 = false;
+                                        boolForNote2 = false;
+                                        break;
+                                    case ConsoleKey.N:
+                                        monthLevel.MonthNumber = DateTime.Now.Month;
+                                        monthLevel.Year = DateTime.Now.Year;
+                                        boolForNote1 = false;
+                                        boolForNote2 = false;
+                                        break;
+                                    case ConsoleKey.X:
+                                        specialList.UserDay.RemoveAll(x => x.Day == userDate.Day &&
+                                        x.Month == userDate.Month && x.Year == userDate.Year);
+                                        boolForNote2 = false;
+                                        break;
 
+                                }
+
+                            }
                         }
+                        
                         break;
+                        
                     case ConsoleKey.Escape:
                         stillWatch = false;
                         break;
@@ -192,26 +200,56 @@ namespace Net14
 
         }
 
-        public static MonthLevel CheckCalendar(ListForNotes list, MonthLevel create, List<SpecialDay> count1)
+        private static ListForNotes AddNotes(ListForNotes List, DateTime Date, string note, string noNotes)
         {
-            var creater = create;
+            var specialList = List;
+            List<SpecialDay> findNote = specialList.UserDay.FindAll(x => x.Day == Date.Day &&
+                                x.Month == Date.Month && x.Year == Date.Year && x.Note != noNotes);
+            if (findNote.Count != 0)
+            {
+                findNote[0].Note += "\n" + note;
+                findNote[0].Color = ConsoleColor.Green;
+            }
+            else
+            {
+                var datee = new SpecialDay()
+                {
+                    Day = Date.Day,
+                    Month = Date.Month,
+                    Year = Date.Year,
+                    Note = note
+                };
+                specialList.UserDay.Add(datee);
+            }
+            return specialList;
+        }
+
+        public static MonthLevel CheckCalendar(ListForNotes list, MonthLevel crea, string noNotes)
+        {
+            var creator = crea;
             var specialList = list;
-            List<SpecialDay> count = count1;
+            /*specialList.UserDay.RemoveAll(cell => cell.Note == noNotes);*/
+            for (int i = 0; i<crea.Month.Count; i++)
+            {
+                crea.Month[i].Note = noNotes;
+            }
+            List<SpecialDay> count = specialList.UserDay.FindAll(cell => cell.Year == creator.Year && cell.Month == creator.MonthNumber); 
             if (count.Count != 0)
             {
                 for (int x = 0; x < count.Count; x++)
                 {
-                    for (int y = 0; y < creater.Month.Count; y++)
+                    for (int y = 0; y < creator.Month.Count; y++)
                     {
-                        if ( count[x].Day.ToString() == create.Month[y].Symbol)
+                        if ( count[x].Day.ToString() == creator.Month[y].Symbol)
                         {
-                            create.Month[y].Note = count[x].Note;
-                            create.Month[y].Color = count[x].Color;
+                            creator.Month[y].Note = count[x].Note;
+                            creator.Month[y].Color = count[x].Color;
                         }
+                        
                     }
                 }
             }
-            return creater;
+            return count.Count == 0 ? crea : creator;
         }
 
         private static DateTime EnterDate(string mess)
