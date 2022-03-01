84729c09d249d065abea8f7b91dd62e69f64ec81
ANTON <tse.toneee@gmail.com>
2022-03-01 21:18:42 +0300

    `

---
+++ Net14/Net14.Web/Controllers/TeamStoreController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Net14.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net14.Web.Controllers
+{
+    public class TeamStoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
