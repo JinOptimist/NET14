a7cda627ec59da6a2ad26fcd5789b0706c24bd9f
Kimgim007 <dimomaks1@gmail.com>
2022-03-04 20:02:30 +0300

    Add calculator

---
+++ Net14/Net14.Web/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Net14.Web.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
