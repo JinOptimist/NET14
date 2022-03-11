451b558e2db3bf55bde079fa92ff660c672739e0
Журавлев Алексей <alehandro.200351@gmail.com>
2022-03-11 21:17:27 +0300

    Fix

--- Net14/TeamSocial/Models/User.cs
+++ Net14/TeamSocial/Models/User.cs
@@ -15,7 +15,7 @@ namespace SocialWeb
         public string City { get; set; }
 
         public byte[] Salt { get; set; }
-        public DateTime DateOfRegistration { get; } = DateTime.Now.ToLocalTime();
+        public DateTime DateOfRegistration { get; set; } = DateTime.Now.ToLocalTime();
 
         public Wall wallOfUser = new Wall();
         public List<User> friends { get; set; } = new List<User>();
