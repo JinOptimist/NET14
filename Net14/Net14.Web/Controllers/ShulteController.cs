793a1511ea4d59a3da554a08ad5c8a24aaf06731
Kirill Perepechkin <perepechkin.kirill.2002.money@gmail.com>
2022-03-13 22:10:11 +0300

    Add shulte

---
+++ Net14/Net14.Web/Controllers/ShulteController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Net14.Web.Models.Shulte;
+using System;
+using System.Collections.Generic;
+
+namespace Net14.Web.Controllers
+{
+    public class ShulteController : Controller
+    {
+        public IActionResult Main()
+        {
+            var number = new RandomNumberViewModel();
+            var random = new Random();
+            
+                number.Number1 = random.Next(1, 25);
+                number.Number2 = random.Next(1, 25);
+                number.Number3 = random.Next(1, 25);
+                number.Number4 = random.Next(1, 25);
+                number.Number5 = random.Next(1, 25);
+                number.Number6 = random.Next(1, 25);
+                number.Number7 = random.Next(1, 25);
+                number.Number8 = random.Next(1, 25);
+                number.Number9 = random.Next(1, 25);
+                number.Number10 = random.Next(1, 25);
+                number.Number11 = random.Next(1, 25);
+                number.Number12 = random.Next(1, 25);
+                number.Number13 = random.Next(1, 25);
+                number.Number14 = random.Next(1, 25);
+                number.Number15 = random.Next(1, 25);
+                number.Number16 = random.Next(1, 25);
+                number.Number17 = random.Next(1, 25);
+                number.Number18 = random.Next(1, 25);
+                number.Number19 = random.Next(1, 25);
+                number.Number20 = random.Next(1, 25);
+                number.Number21 = random.Next(1, 25);
+                number.Number22 = random.Next(1, 25);
+                number.Number23 = random.Next(1, 25);
+                number.Number24 = random.Next(1, 25);
+            
+            return View(number);
+           
+        }
+    }
+}
---
+++ Net14/Net14.Web/Models/Shulte/RandomNumberViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Net14.Web.Models.Shulte
+{
+    public class RandomNumberViewModel
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        public int Number3 { get; set; }
+        public int Number4 { get; set; }
+        public int Number5 { get; set; }
+        public int Number6 { get; set; }
+        public int Number7 { get; set; }
+        public int Number8 { get; set; }
+        public int Number9 { get; set; }
+        public int Number10 { get; set; }
+        public int Number11 { get; set; }
+        public int Number12 { get; set; }
+        public int Number13 { get; set; }
+        public int Number14 { get; set; }
+        public int Number15 { get; set; }
+        public int Number16 { get; set; }
+        public int Number17 { get; set; }
+        public int Number18 { get; set; }
+        public int Number19 { get; set; }
+        public int Number20 { get; set; }
+        public int Number21 { get; set; }
+        public int Number22 { get; set; }
+        public int Number23 { get; set; }
+        public int Number24 { get; set; }
+
+
+
+    }
+}
