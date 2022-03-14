0af9c02f4183e1aa8f9a00f7a397aa1a33e37a1c
Kirill Perepechkin <perepechkin.kirill.2002.money@gmail.com>
2022-03-14 20:30:09 +0300

    Add border for table

--- Net14/Net14.Web/Controllers/ShulteController.cs
+++ Net14/Net14.Web/Controllers/ShulteController.cs
@@ -10,33 +10,8 @@ namespace Net14.Web.Controllers
         public IActionResult Main()
         {
             var number = new RandomNumberViewModel();
-            var random = new Random();
-            
-                number.Number1 = random.Next(1, 25);
-                number.Number2 = random.Next(1, 25);
-                number.Number3 = random.Next(1, 25);
-                number.Number4 = random.Next(1, 25);
-                number.Number5 = random.Next(1, 25);
-                number.Number6 = random.Next(1, 25);
-                number.Number7 = random.Next(1, 25);
-                number.Number8 = random.Next(1, 25);
-                number.Number9 = random.Next(1, 25);
-                number.Number10 = random.Next(1, 25);
-                number.Number11 = random.Next(1, 25);
-                number.Number12 = random.Next(1, 25);
-                number.Number13 = random.Next(1, 25);
-                number.Number14 = random.Next(1, 25);
-                number.Number15 = random.Next(1, 25);
-                number.Number16 = random.Next(1, 25);
-                number.Number17 = random.Next(1, 25);
-                number.Number18 = random.Next(1, 25);
-                number.Number19 = random.Next(1, 25);
-                number.Number20 = random.Next(1, 25);
-                number.Number21 = random.Next(1, 25);
-                number.Number22 = random.Next(1, 25);
-                number.Number23 = random.Next(1, 25);
-                number.Number24 = random.Next(1, 25);
-            
+            number.Today = (int)DateTime.Now.DayOfWeek;
+
             return View(number);
            
         }
