77234e206380c61663be28c3245652ac44f18113
Артём Клюенков <klarse4ever@gmail.com>
2022-03-15 19:44:11 +0300

    update calendar(html)

---
+++ Net14/Net14.Web/EfStuff/DbModel/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net14.Web.EfStuff.DbModel
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public string UserName { get; set; }
+        public int Importance { get; set; }
+        public string DateCreate { get; set; }
+        public DateTime DateUpdate { get; set; }
+        public string Date { get; set; }
+    }
+}
