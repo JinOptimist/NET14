cdcca0f89fe9fe00bb4d1e58df6ff461fb86b613
andrejdesukevic@gmail.com <andrejdesukevic@gmail.com>
2022-03-18 00:49:32 +0300

    NewImage

--- Net14/Net14.Web/EfStuff/DbModel/Product.cs
+++ Net14/Net14.Web/EfStuff/DbModel/Product.cs
@@ -10,7 +10,7 @@ namespace Net14.Web.EfStuff.DbModel
         public int Id { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
-        public int Category { get; set; }
+        public string Category { get; set; }
         public int Quantity { get; set; }
         public string Material { get; set; }
         public int Price { get; set; }
