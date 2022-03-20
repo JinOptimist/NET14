539f4f72e4864829b0545205f2db6fc5a1e2579d
Журавлев Алексей <alehandro.200351@gmail.com>
2022-03-20 19:35:16 +0300

    fix

--- Net14/Net14.Web/wwwroot/js/Shulte/button.js
+++ Net14/Net14.Web/wwwroot/js/Shulte/button.js
@@ -1,8 +1,30 @@
-﻿$(document).ready(function()
+﻿let start = 1;
+
+$(document).ready(function ()
 {
     $(".button").click(function ()
     {
-        $(this).css("opacity", "0.1");
+        if ($(this).css("opacity") == "0.1")
+        {
+            return;
+        }
+        if (($(this).text().trim()) != String(start)) {
+            $(this).css("background-color", "rgba(255, 0, 0, 0.5)")
+            setTimeout(() => {
+                $(this).css("background-color", "white")
+            }, 500);
+        }
+        else if (($(this).text().trim()) == String(start))
+        {
+            if (($(this).text().trim()) == String(24))
+            {
+                start = 1;
+                location.reload(); //Когда доходим до 24, то страница перезагрузится 
+            }
+            $(this).css("opacity", "0.1");
+            start += 1;
+        }
+        
     })
 })
 //$(this).css("opacity", "0.1");
