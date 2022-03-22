5551ab0397df5f916cc3a102b543beb0de8d00ef
Kimgim007 <dimomaks1@gmail.com>
2022-03-22 21:28:18 +0300

    Fix3

---
+++ Net14/Net14.Web/EfStuff/DbModel/ArtImage/ArtImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net14.Web.EfStuff.DbModel
+{
+    public class ArtImage 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int Rate { get; set; }
+
+    }
+}
