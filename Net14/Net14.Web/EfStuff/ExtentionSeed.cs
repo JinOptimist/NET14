ccf94683b7fbbb7235b49c35a741b24eb7817092
JinOptimist <88jin@mail.ru>
2022-03-29 19:40:02 +0300

    Add ExSeed

---
+++ Net14/Net14.Web/EfStuff/ExtentionSeed.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Net14.Web.EfStuff.DbModel;
+using Net14.Web.EfStuff.Repositories;
+
+namespace Net14.Web.EfStuff
+{
+    public static class ExtentionSeed
+    {
+        public static IHost Seed(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+
+                SeedImage(scope);
+            }
+
+            return host;
+        }
+
+        private static void SeedImage(IServiceScope scope)
+        {
+            var imageRepository = scope.ServiceProvider.GetService<ImageRepository>();
+
+            if (!imageRepository.Any())
+            {
+                var image = new Image()
+                {
+                    Name = "qwe",
+                    Url = "qwe",
+                    Rate = 99
+                };
+
+                imageRepository.Save(image);
+            }
+        }
+    }
+}
