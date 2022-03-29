601e8795ed1a31fdf1020e5780f0de6c81a52539
Kirill Perepechkin <perepechkin.kirill.2002.money@gmail.com>
2022-03-29 23:38:34 +0300

    Add Shulte game in Social

---
+++ Net14/Net14.Web/Models/Shulte/RandomNumberViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Net14.Web.Models.Shulte
+{
+    public class RandomNumberViewModel
+    {
+        public List<int> Random()
+        {
+            var array = Enumerable.Range(1, 24).ToList();
+            var random = new Random();
+            for (int i = array.Count - 1; i >= 0; i--)
+            {
+                int j = random.Next(i);
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+            return array;
+
+        }
+    }
+}
