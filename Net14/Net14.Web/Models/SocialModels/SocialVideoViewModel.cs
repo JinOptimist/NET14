4b1c1278e47d150b2b0fdfb6fade3c2c13f47fd4
Журавлев Алексей <alehandro.200351@gmail.com>
2022-03-31 14:59:55 +0300

    Test

---
+++ Net14/Net14.Web/Models/SocialModels/SocialVideoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net14.Web.Models
+{
+    public class SocialVideoViewModel
+    {
+        public string VideoId { get; set; }
+        public string VideoDescription { get; set; }
+        public string VideoPhoto { get; set; }
+        public DateTime DateOfPosting { get; set; }
+
+
+    }
+}
---
+++ Net14/Net14.Web/Services/YouTubeVideoGetter.cs
@@ -0,0 +1,54 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Net14.Web.EfStuff.DbModel.SocialDbModels;
+
+namespace Net14.Web.Services
+{
+    public class YouTubeVideoGetter
+    {
+        public Task<List<SearchResult>> GetVideosFromChannelAsync(string ytChannelId)
+        {
+
+            return Task.Run(() =>
+            {
+                List<SearchResult> res = new List<SearchResult>();
+
+                var _youtubeService = new YouTubeService(new BaseClientService.Initializer()
+                {
+                    ApiKey = "AIzaSyAuB3O2VayUYQct_x1aJBllRxpQf2TMkgA",
+                    ApplicationName = "SocialWeb"//this.GetType().ToString()
+                });
+
+                string nextpagetoken = " ";
+
+                while (nextpagetoken != null)
+                {
+                    var searchListRequest = _youtubeService.Search.List("snippet");
+                    searchListRequest.MaxResults = 50;
+                    searchListRequest.Type = "video";
+                    searchListRequest.ChannelId = ytChannelId;
+                    searchListRequest.PageToken = nextpagetoken;
+
+                    // Call the search.list method to retrieve results matching the specified query term.
+                    var searchListResponse = searchListRequest.Execute();
+
+                    // Process  the video responses 
+                    res.AddRange(searchListResponse.Items);
+
+                    nextpagetoken = searchListResponse.NextPageToken;
+
+                }
+
+                return res;
+
+            });
+        }
+
+    }
+}
+
