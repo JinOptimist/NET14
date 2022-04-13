6795ee167c79e5c996016710573ce3e8557c9409
Журавлев Алексей <alehandro.200351@gmail.com>
2022-04-14 02:15:24 +0300

    Add authentification attributes

--- Net14/Net14.Web/Models/SocialModels/SocialUserRegistrationViewModel.cs
+++ Net14/Net14.Web/Models/SocialModels/SocialUserRegistrationViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Net14.Web.Models.CustomValidationAttribute;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,11 +10,11 @@ namespace Net14.Web.Models
     public class SocialUserRegistrationViewModel
     {
         [Required(ErrorMessage = "Enter Email!")]
+        [Email]
         public string Email { get; set; }
-        [DataType(DataType.Password)]
         [Required(ErrorMessage = "Enter Password!")]
+        [Password]
         public string Password { get; set; }
-        [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Passwords are't the same")]
         [Required(ErrorMessage ="Confirm password!")]
         public string ConfirmPassword { get; set; }
@@ -22,10 +23,13 @@ namespace Net14.Web.Models
         [Required(ErrorMessage = "Enter Country!")]
         public string Country { get; set; }
         [Required(ErrorMessage = "Enter Age!")]
+        [IsPositive]
         public int Age { get; set; }
         [Required(ErrorMessage = "Enter FirstName!")]
+        [Name]
         public string FirstName { get; set; }
         [Required(ErrorMessage = "Enter LastName!")]
+        [Name]
         public string LastName { get; set; }
     }
 }
