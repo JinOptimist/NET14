f3ff6dac295e89094700855125564cabb8ee0be5
Kirill Perepechkin <perepechkin.kirill.2002.money@gmail.com>
2022-05-01 12:18:18 +0300

    Add js for English

---
+++ Net14/Net14.Web/Controllers/SocialEnglishController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Net14.Web.EfStuff;
+using Net14.Web.EfStuff.DbModel.SocialDbModels;
+using Net14.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Net14.Web.EfStuff.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Net14.Web.Services;
+using AutoMapper;
+using Net14.Web.EfStuff.DbModel.SocialDbModels.SocialEnums;
+using Net14.Web.Controllers.AutorizeAttribute;
+
+namespace Net14.Web.Controllers
+{
+    public class SocialEnglishController : Controller
+
+    {
+        private IMapper _mapper;
+
+        public SocialEnglishController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public IActionResult WordsEnglish()
+        {
+            return View();
+        }
+    }
+}
---
+++ Net14/Net14.Web/wwwroot/js/EnglishJS/WordsEnglish.js
@@ -0,0 +1,64 @@
+﻿$(document).ready(function () {
+    const wordsInEnglish =
+        [
+            'sun',
+            'apple',
+            'go',
+            'friend',
+            'name',
+            'play'
+        ];
+    const wordsInRussian =
+        [
+            'солнце',
+            'яблоко',
+            'идти',
+            'друг',
+            'имя',
+            'играть'
+        ];
+
+    init();
+
+    $('.card-block').click(function () {
+        $(this).toggleClass('pressed-card-block')
+    })
+
+    function init() {
+        let wordsSource = wordsInEnglish.concat(wordsInRussian);
+
+        wordsSource = Shuffle(wordsSource);
+
+        CreateCardWithWord(wordsSource);
+    }
+    function CreateCardWithWord(words) {
+        for (var i = 0; i < words.length; i++) {
+
+            var word = words[i];
+
+            const cardBlock = $('<div>');
+            cardBlock.text(word)
+            cardBlock.addClass('card-block');
+            cardBlock.addClass('pressed-card-block')
+            $('.word-cards').append(cardBlock);
+
+        }
+    }
+    function Shuffle(array) {
+        var m = array.length, t, i;
+
+        // While there remain elements to shuffle…
+        while (m) {
+
+            // Pick a remaining element…
+            i = Math.floor(Math.random() * m--);
+
+            // And swap it with the current element.
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
+
+        return array;
+    }
+});
