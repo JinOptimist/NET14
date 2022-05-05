c3bf82b7fa7570e28026454091e1905d2f11b10a
JinOptimist <88jin@mail.ru>
2022-05-05 22:04:57 +0300

    Add slow animation

--- Net14/Net14.Web/wwwroot/js/gallery/carusel.js
+++ Net14/Net14.Web/wwwroot/js/gallery/carusel.js
@@ -1,7 +1,11 @@
 ﻿$(document).ready(function () {
     const animationSpeed = 1 * 1000;
     const autoClickTime = 3 * 1000;
+
     const currentImageWidth = 200;
+    const paddingCurrent = 5;
+    const paddingStep1 = 3;
+    const paddingStep2 = 0;
 
     let isAnimationActive = false;
     let isHovered = false;
@@ -54,32 +58,37 @@
 
         if (!isForwardDirection) {
             $('.prev-step-2').animate({
-                width: currentImageWidth / 2
+                width: currentImageWidth / 2,
+                padding: paddingStep1
             }, animationSpeed);
         }
         const prevStep1Width = isForwardDirection
-            ? 1
+            ? 0
             : currentImageWidth;
         $('.prev-step-1').animate({
-            width: prevStep1Width
+            width: prevStep1Width,
+            padding: isForwardDirection ? paddingStep2 : paddingCurrent
         }, animationSpeed);
 
         const nextStep1Width = isForwardDirection
             ? currentImageWidth
-            : 1;
+            : 0;
         $('.next-step-1').animate({
-            width: nextStep1Width
+            width: nextStep1Width,
+            padding: isForwardDirection ? paddingCurrent : paddingStep2 
         }, animationSpeed);
 
         if (isForwardDirection) {
             $('.next-step-2').animate({
-                width: currentImageWidth / 2
+                width: currentImageWidth / 2,
+                padding: paddingStep1 
             }, animationSpeed);
         }
 
         $('.current').animate(
             {
-                width: currentImageWidth / 2
+                width: currentImageWidth / 2,
+                padding: paddingStep1
             },
             animationSpeed,
             'swing',
@@ -98,13 +107,14 @@
     }
 
     function setDefaultStyle() {
-        $('.current').css('width', 200);
+        $('.current').css('width', currentImageWidth);
+        $('.current').css('padding', paddingCurrent);
 
-        $('.prev-step-1').css('width', 100);
-        $('.next-step-1').css('width', 100);
+        $('.prev-step-1,.next-step-1').css('width', currentImageWidth / 2);
+        $('.prev-step-1,.next-step-1').css('padding', paddingStep1);
 
-        $('.prev-step-2').css('width', 1);
-        $('.next-step-2').css('width', 1);
+        $('.prev-step-2,.next-step-2').css('width', 0);
+        $('.prev-step-2,.next-step-2').css('padding', paddingStep2);
     }
 
     function updateImages() {
