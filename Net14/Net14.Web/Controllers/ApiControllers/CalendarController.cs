81537c1c0c5841445d4cee65ee62b975244db967
Артём Клюенков <klarse4ever@gmail.com>
2022-05-18 11:28:20 +0300

    fix

---
+++ Net14/Net14.Web/Controllers/ApiControllers/CalendarController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Net14.Web.EfStuff.DbModel.SocialDbModels.SocialEnums;
+using Net14.Web.EfStuff.Repositories;
+using Net14.Web.Models.Calendar;
+using Net14.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net14.Web.Controllers.ApiControllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CalendarController : ControllerBase
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private DaysNoteRepository _daysNoteRepository;
+        private UserService _userService;
+
+        public CalendarController(IWebHostEnvironment webHostEnvironment,
+            DaysNoteRepository daysNoteRepository, UserService userService)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _daysNoteRepository = daysNoteRepository;
+            _userService = userService;
+        }
+      
+    }
+}
