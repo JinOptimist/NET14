d2533b24c6d4d90531763877b51637ea6e84d538
Kirill <perepechkin_2002@mail.ru>
2022-05-28 12:38:19 +0300

    add database for to do list

---
+++ Net14/Net14.Web/EfStuff/DbModel/SocialDbModels/FoldersForToDo - Копировать.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Net14.Web.EfStuff.DbModel.SocialDbModels
+{
+    public class IssuesForToDo : BaseModel
+    {
+        public int IdIssue { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public virtual FoldersForToDo Folder { get; set; }
+        
+    }
+}
