febd630e21aa38ced3cb61e0dd5cfa57861e8086
Kirill <perepechkin_2002@mail.ru>
2022-05-30 13:39:44 +0300

    fix

---
+++ Net14/Net14.Web/Migrations/20220530103702_Fix folders and issues.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Net14.Web.Migrations
+{
+    public partial class Fixfoldersandissues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdIssue",
+                table: "IssuesForToDo");
+
+            migrationBuilder.DropColumn(
+                name: "CountIssues",
+                table: "FoldersForToDo");
+
+            migrationBuilder.DropColumn(
+                name: "IdFolder",
+                table: "FoldersForToDo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IdIssue",
+                table: "IssuesForToDo",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CountIssues",
+                table: "FoldersForToDo",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IdFolder",
+                table: "FoldersForToDo",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
