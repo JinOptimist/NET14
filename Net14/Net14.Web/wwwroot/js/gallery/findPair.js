99bf8649d15220a8a9d307fac87ef641248b319d
JinOptimist <88jin@mail.ru>
2022-06-02 21:28:26 +0300

    Add animation for rotation

--- Net14/Net14.Web/wwwroot/js/gallery/findPair.js
+++ Net14/Net14.Web/wwwroot/js/gallery/findPair.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-
+    const rotationSpeed = 1500;
     let activeImageIndex;
 
     let imageUrls = [];
@@ -7,13 +7,17 @@
     init();
 
     function init() {
-        $.get('/Gallery/UrlsForFindPair')
+        $.get('/api/Gallery/GetGirlImages')
             .done(function (urls) {
                 imageUrls = urls.concat(urls);
 
                 imageUrls = shuffle(imageUrls);
 
                 createImageBlocks(imageUrls);
+
+                showAllImages();
+
+                setTimeout(hideAllImages, rotationSpeed * 2 + 1000);
             });
     }
 
@@ -56,11 +60,12 @@
     }
 
     function onImageBlockClick() {
+        const currentCrad = $(this);
         const isBackside = $(this).hasClass('backside');
         //if clicked image is open
         if (!isBackside) {
             activeImageIndex = undefined;
-            $(this).addClass('backside');
+            hideCard(currentCrad);
             return;
         }
 
@@ -68,7 +73,8 @@
         //if there is no active image
         if (!activeImageIndex) {
             activeImageIndex = clickedImageIndex;
-            $(this).removeClass('backside');
+
+            showCard(currentCrad);
             return;
         }
 
@@ -76,6 +82,7 @@
         const clickedImageUrl = imageUrls[clickedImageIndex];
         if (activeImageUrl == clickedImageUrl) {
             //if pair was founded
+
             $(`[data-index=${activeImageIndex}]`).addClass('complete');
             $(`[data-index=${activeImageIndex}]`).removeClass('backside');
             $(`[data-index=${clickedImageIndex}]`).addClass('complete');
@@ -84,12 +91,72 @@
         } else {
             //if fail
             activeImageIndex = undefined;
-            $(`[data-index=${clickedImageIndex}]`).removeClass('backside');
-            setTimeout(hideAllImages, 500);
+            showCard($(`[data-index=${clickedImageIndex}]`));
+            setTimeout(hideAllImages, rotationSpeed * 2 + 500);
         }
     }
 
+    function showAllImages(){
+        const openCards = $('.image-block.backside:not(.complete)');
+        openCards.each(function () {
+            if ($(this).hasClass('backside')) {
+                showCard($(this));
+            }
+        });
+    }
+
     function hideAllImages() {
-        $('.image-block:not(.complete)').addClass('backside');
+        const openCards = $('.image-block:not(.complete)');
+        openCards.each(function () {
+            if (!$(this).hasClass('backside')) {
+                hideCard($(this));
+            }
+        });
+    }
+
+    function showCard(currentCrad) {
+        rotate(currentCrad, 0, 90, () => {
+            currentCrad.removeClass('backside');
+            rotate(currentCrad, -90, 0, null, true);
+        }, true);
+    }
+
+    function hideCard(currentCrad) {
+        rotate(currentCrad, 0, 90, () => {
+            currentCrad.addClass('backside');
+            rotate(currentCrad, -90, 0, null, false);
+        }, false);
+    }
+
+    function rotate(currentCrad, startDeg, endDeg, animationCompleteFunc, isLeft) {
+        const cardWidth = currentCrad.width();
+        currentCrad.css('transform', 'rotateY(' + startDeg + 'deg)');
+        currentCrad.animate(
+            {
+                transformRotateIndex: endDeg /* или любое другое не очень-то нужное здесь свойство */
+            },
+            {
+                step: function (now, fx) {
+                    currentCrad.css('transform', 'rotateY(' + now + 'deg)');
+                    const sinValue = Math.sin(now / 180);
+                    const deltaWidth = cardWidth * sinValue;
+                    if (isLeft) {
+                        currentCrad.css('right', 'auto');
+                        currentCrad.css('left', deltaWidth);
+                    } else {
+                        currentCrad.css('left', 'auto');
+                        currentCrad.css('right', deltaWidth);
+                    }
+                },
+                duration: rotationSpeed,
+                complete: function () {
+                    //currentCrad.removeClass('backside'); 
+                    if (animationCompleteFunc) {
+                        animationCompleteFunc();
+                    }
+                }
+            },
+            'swing'
+        );
     }
 });
