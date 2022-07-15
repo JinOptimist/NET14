f008dbe5547746bb2f3c223c1bd4632599a5577d
Андрей Десюкевич <70821681+AndreyDesukevic@users.noreply.github.com>
2022-07-15 18:08:08 +0300

    a

--- Net14/Net14.Web/wwwroot/js/StoreJs/Checkout.js
+++ Net14/Net14.Web/wwwroot/js/StoreJs/Checkout.js
@@ -2,17 +2,21 @@
     $(".delivery").hide();
 
     $('input[type="radio"]').click(function () {
+        var sum = $('input[name="Sum"]').val();
+        const costDelivery = 5;
         if ($(this).attr("value") == "Pickup") {
             $(".box").hide();
             $(".pickup").show();
             $('input[name="CheckedDeliveryAddressId"]').prop('checked', false);
+            $('input[name="Sum"]').val(sum)
         }
         if ($(this).attr("value") == "Delivery") {
             $(".box").hide();
             $(".delivery").show();
             $('input[name="CheckedStoreAddressId"]').prop('checked', false);
+            $('input[name="Sum"]').val(sum + costDelivery)
         }
-       
-        
+
+
     });
 });
